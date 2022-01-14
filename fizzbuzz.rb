fdd22ea77ebfc8725cbec9cd848995075cdf0fde
jparsons-git <jane.parsons@waitrose.co.uk>
2022-01-14 10:52:35 +0000

    Finish adding the fizzbuzz outputs

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,14 @@
 # write fizzbuzz code here
 
 (1..20).each do |i|
-  if i % 3 == 0
+  
+  if i % 3 == 0 && i % 5 == 0   # if divisible by 3 AND 5, use "FizzBuzz"
+    puts "FizzBuzz"    
+  elsif i % 3 == 0              # if only divisble by 3, use "Fizz"
     puts "Fizz"
-  elsif i % 5 == 0
+  elsif i % 5 == 0              # if only divisble by 5, use "Buzz"
     puts "Buzz"
-  end
+  else                          # anything else, use the number
+    puts i 
+  end    
 end
